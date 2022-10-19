cbe09e28dbf1f49b057231bbc17fe297e0d1ac57
Ishan Chandran <Ishan.Chandran@students.rsd.edu>
2022-10-19 15:49:04 -0700

    Add project files.

---
+++ Hyperoperation calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Hyperoperation_calculator
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.WriteLine(HyperOperate(3, 5, 3));
+		}
+
+		static long HyperOperate(long input, short order, short power)
+		{
+			long output = input;
+			order = (short)(order - 2);
+			for(int i = 0; i < power; i++)
+			{
+				output = (long)Math.Pow(output, input);
+			}
+			
+				if (order > 3) {
+					HyperOperate(output, (short)(order - 1), power);
+				}
+			output = (long)Math.Pow(output, input);
+
+			return output;
+		}
+	}
+}
